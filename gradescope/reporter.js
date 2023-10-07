41fef5bd162b892ddc3a734737b8cf1d1075dcc6
Joseph Muller <j.muller@bbk.ac.uk>
2023-10-07 16:40:46 +0100

    Modify reporter so that every homework is 100/100

--- gradescope/reporter.js
+++ gradescope/reporter.js
@@ -4,16 +4,19 @@ export default class GradescopeReporter extends JsonReporter {
   async addGradescopeProperties (reportString) {
     const report = JSON.parse(reportString)
     report.tests = []
-    report.score = 10
+    let numPassed = 0
+    let total = 0
     for (const resultSet in report.testResults) {
       for (const test in report.testResults[resultSet].assertionResults) {
         const result = report.testResults[resultSet].assertionResults[test]
-        if (!(result.status === 'passed')) report.score -= 1
+        if (result.status === 'passed') numPassed += 1
+        total += 1
         report.testResults[resultSet].assertionResults[test].name = result.fullName
         delete report.testResults[resultSet].assertionResults[test].fullName
         report.tests.push(report.testResults[resultSet].assertionResults[test])
       }
     }
+    report.score = 100 * numPassed / total
     delete report.testResults
     return JSON.stringify(report, null, 2)
   }
