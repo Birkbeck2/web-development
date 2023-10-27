cf560eb9d4944dcafd39b8e1f06476a04caba730
Joseph Muller <j.muller@bbk.ac.uk>
2023-10-27 09:45:34 +0100

    Improve tests

--- workshops/semantic-html/trading-places.test.js
+++ workshops/semantic-html/trading-places.test.js
@@ -3,6 +3,10 @@
 import { describe, expect, test } from 'vitest'
 import { findSubmissions, validateHtml, loadDoc } from '../test-utils.js'
 import { parse, isAbsolute } from 'node:path'
+import { readFileSync } from 'node:fs'
+import url from 'node:url'
+import { createHash } from 'node:crypto'
+import sizeOf from 'image-size'
 
 const submissionFile = 'trading-places.html'
 
@@ -44,15 +48,22 @@ describe(submissionFile, async () => {
           })
         })
       })
-      test('images have dimensions', async () => {
+      test('images have dimensions', () => {
         const imgs = doc.querySelectorAll('img')
         const imgsWithHeightAndWidth = Array.from(imgs).filter(img => {
           return img.height && img.width
         })
         expect(imgsWithHeightAndWidth).toHaveLength(3)
+        imgsWithHeightAndWidth.forEach(img => {
+          const imgUrl = new url.URL(img.src)
+          const dimensions = sizeOf(imgUrl.pathname)
+          expect(img.width).toEqual(dimensions.width)
+          expect(img.height).toEqual(dimensions.height)
+        })
       })
       test('images are in subfolder', () => {
         const imgs = doc.querySelectorAll('img')
+        expect(Array.from(imgs).length).toBeGreaterThan(0)
         Array.from(imgs).forEach(img => {
           const pathObj = parse(img.getAttribute('src'))
           expect(pathObj.dir.length).toBeGreaterThan(1)
@@ -60,18 +71,39 @@ describe(submissionFile, async () => {
       })
       test('image filepaths are relative', () => {
         const imgs = doc.querySelectorAll('img')
+        expect(Array.from(imgs).length).toBeGreaterThan(0)
         Array.from(imgs).forEach(img => {
           expect(isAbsolute(img.getAttribute('src'))).toBeFalsy()
         })
       })
       test('links and filepaths are well formed', () => {
         const imgs = doc.querySelectorAll('img')
+        expect(Array.from(imgs).length).toBeGreaterThan(0)
         Array.from(imgs).forEach(img => {
           expect(img.getAttribute('src')).not.toMatch(/ |_/)
         })
         const as = doc.querySelectorAll('a')
+        expect(Array.from(as).length).toBeGreaterThan(0)
         Array.from(as).forEach(a => {
-          expect(a.href).not.toMatch(/ /)
+          const href = a.getAttribute('href')
+          expect(href.length).toBeGreaterThan(0)
+          console.log(href)
+          console.log(href.length)
+        })
+      })
+      test('images load correctly', async () => {
+        const imgs = doc.querySelectorAll('img')
+        expect(Array.from(imgs).length).toBeGreaterThan(0)
+        const checksums = [
+          '090147bda9b96bebacaf595bbcb0f142',
+          '471ca16ecfc4c339ad699a6a618a233c',
+          'f4e0ea63e8cb0823577481ebd59d0d43'
+        ]
+        Array.from(imgs).forEach(async img => {
+          const imgUrl = new url.URL(img.src)
+          const content = readFileSync(imgUrl.pathname)
+          const hash = createHash('md5').update(content).digest('hex')
+          expect(checksums).toContain(hash)
         })
       })
       test('links in the top region of the page', () => {
