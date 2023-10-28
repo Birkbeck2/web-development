5488bb9149aec9c9e3db4b34cfd28ea269741255
Joseph Muller <j.muller@bbk.ac.uk>
2023-10-28 13:44:04 +0100

    Use PNPM on Gradescope servers

--- gradescope/setup.sh
+++ gradescope/setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 apt-get install apt-utils
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
-apt-get install -y nodejs
-apt-get install -y npm
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+PATH=/root/.local/share/pnpm/:$PATH
+
 cp /autograder/source/package.json /autograder/package.json
-cd /autograder/ && npm install
+cd /autograder/ && pnpm install
