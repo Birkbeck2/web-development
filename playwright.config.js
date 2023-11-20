fcced757d46e975b2e7eb3d0f63be45a8afbe445
Joseph Muller <j.muller@bbk.ac.uk>
2023-11-20 00:49:18 +0000

    Small fixes

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', { outputFile: './results/results.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
