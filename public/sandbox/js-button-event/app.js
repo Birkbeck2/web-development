dfc4263374ff2fae00e8c2791c3dabc7e48e41dc
Joseph Muller <j.muller@bbk.ac.uk>
2025-03-07 14:28:25 +0000

    Update events materials

--- public/sandbox/js-button-event/app.js
+++ public/sandbox/js-button-event/app.js
@@ -1,7 +1,7 @@
-let buttonWrapper = document.querySelectorAll('#button-wrapper')
+let buttonWrapper = document.querySelector('#button-wrapper')
 
-function changeColor(event) {}
-  let color = event.value
+function changeColor(event) {
+  let color = event.target.value
   let square = document.querySelector('.square')
   square.className = `square ${color}`
   square.innerHTML = `<div class="color-label">${color}</div>`
