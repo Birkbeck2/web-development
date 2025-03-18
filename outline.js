5f0dc66135da3ac2b4c15818943fe15315db2b3d
[HelenaHW] <[h.wee@bbk.ac.uk]>
2025-03-18 16:58:18 +0000

    outline.js

--- outline.js
+++ outline.js
@@ -297,6 +297,25 @@ export const outline = [
       },
   ]
   },
+  {
+    text: 'Animating Data',
+    collapsed: true,
+    lecturer: 'Helena Wee',
+    items: [
+      {
+        text: 'Prep',
+        link: '/lectures/animating-data.html',
+      },
+      {
+        text: 'Animating Data',
+        link: '/lectures/animating-data-1.html',
+      },
+      {
+        text: 'Workshop',
+        link: '/workshops/animating-data/index.html',
+      },
+  ]
+  },
   // {
   //   text: 'Timeouts, Debugging and Packages',
   //   lecturer: 'Helena Wee and Joseph Muller',
