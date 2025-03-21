f4ba68b3bf022103848c2a885e1a471049277543
Joseph Muller <j.muller@bbk.ac.uk>
2025-03-21 14:47:12 +0000

    Add student project links

--- projects.js
+++ projects.js
@@ -3,6 +3,10 @@ export const projects = [
     h2: 'Web Development and User Experience',
     projects: [
       {
+        name: 'Eco Fresh Markt',
+        pages: 'https://birkbeck2.github.io/web-development-website-project-danie-pixel/',
+      },
+      {
         name: 'Maria Jonas Photography',
         pages: 'https://birkbeck2.github.io/web-development-and-user-experience---assessment-02-harshi-chandwani/',
       },
@@ -16,6 +20,19 @@ export const projects = [
   {
     h2: 'Visual Design and Web Project',
     projects: [
+      {
+        name: 'bubbles - adoption service',
+        pages: 'https://birkbeck2.github.io/web-project-example-emma/',
+        code: 'https://github.com/Birkbeck2/web-project-example-emma/',
+      },
+      {
+        name: 'DPixel-Books',
+        pages: 'https://birkbeck2.github.io/web-project-danie-pixel/',
+      },
+      {
+        name: 'BookGlobe',
+        pages: 'https://birkbeck2.github.io/web-project-avauga03/',
+      },
       {
         name: 'Happy Forecast',
         pages: 'https://birkbeck2.github.io/vdwp2223-web-project-harshi-chandwani/',
