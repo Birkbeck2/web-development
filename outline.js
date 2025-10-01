481479f6c0f08f8f2c00c6dcbd76f05388207e69
Joseph Muller <j.muller@bbk.ac.uk>
2025-10-01 20:27:31 +0100

    Hide 2024-25 content

--- outline.js
+++ outline.js
@@ -7,7 +7,7 @@ export const outline = [
   },
   {
     text: 'Markdown and GitHub',
-    collapsed: true,
+    collapsed: false,
     lecturer: 'Joseph Muller',
     items: [
       {
@@ -24,298 +24,298 @@ export const outline = [
       }
     ]
   },
-  {
-    text: 'HTML',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/html-prep.html',
-      },
-      {
-        text: 'Live coding',
-        link: '/lectures/html-live-coding.html',
-      },
-      {
-        text: 'Workshop on HTML',
-        link: '/workshops/html/index.html',
-      }
-    ]
-  },
-  {
-    text: 'Introduction to CSS',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    // concepts: 'stylesheets, style and content, internal and external CSS, selector, declaration, property, value, cascade, fonts, box model, margins, padding, borders, alignment, hover, display',
-    // code: '{ } . # ; " class id color background-color width height text-align text-decoration font-family margin padding border :hover display'
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/intro-css.html',
-      },
-      {
-        text: 'Workshop on Intro to CSS',
-        link: '/workshops/intro-css/index.html',
-      }
-    ]
-  },
-  {
-    text: 'Responsive CSS and flexbox',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    // concepts: 'responsive design, media queries, flexible layouts, grid layouts',
-    // code: 'media flex grid'
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/responsive-css-flexbox.html',
-      },
-      {
-        text: 'Workshop: Responsive CSS and flexbox',
-        link: '/workshops/responsive/index.html',
-      }
-    ]
-  },
-  {
-    text: 'Accessibility in HTML and CSS',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/accessibility-prep.html',
-      },
-      {
-        text: 'Live coding',
-        link: '/lectures/accessibility-live-coding.html',
-      },
-      // {
-      //   text: 'Will it work? Using sandboxes to run code experiments',
-      //   link: '/lectures/will-it-work-using-sandboxes-to-test-code.html',
-      //   concepts: 'sandboxes'
-      // },
-      // {
-      //   text: 'Accessibility',
-      //   link: '/lectures/accessibility.html',
-      //   concepts: 'digital accessibility, disability, mainstream and assistive technology, standards',
-      //   code: ':focus outline header nav main section footer aria-label aria-labelledby'
-      // },
-      // {
-      //   text: 'Forms',
-      //   link: '/lectures/html-forms.html',
-      //   concepts: 'HTTP request methods, front end and back end',
-      //   code: 'form method action type label for input textarea name button'
-      // },
-      {
-        text: 'Workshop: Accessiblity',
-        link: '/workshops/accessibility/index.html',
-      }
-    ]
-  },
-  {
-    text: 'Grids and other CSS concepts',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    //   // concepts: 'grids, positioning, z-index, navigation',
-    //   // code: 'grids positioning navigation'
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/grids-other-css-concepts.html',
-      },
-      {
-        text: 'Workshop: Grids and other CSS concepts',
-        link: '/workshops/grid/index.html',
+  // {
+  //   text: 'HTML',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/html-prep.html',
+  //     },
+  //     {
+  //       text: 'Live coding',
+  //       link: '/lectures/html-live-coding.html',
+  //     },
+  //     {
+  //       text: 'Workshop on HTML',
+  //       link: '/workshops/html/index.html',
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: 'Introduction to CSS',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   // concepts: 'stylesheets, style and content, internal and external CSS, selector, declaration, property, value, cascade, fonts, box model, margins, padding, borders, alignment, hover, display',
+  //   // code: '{ } . # ; " class id color background-color width height text-align text-decoration font-family margin padding border :hover display'
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/intro-css.html',
+  //     },
+  //     {
+  //       text: 'Workshop on Intro to CSS',
+  //       link: '/workshops/intro-css/index.html',
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: 'Responsive CSS and flexbox',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   // concepts: 'responsive design, media queries, flexible layouts, grid layouts',
+  //   // code: 'media flex grid'
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/responsive-css-flexbox.html',
+  //     },
+  //     {
+  //       text: 'Workshop: Responsive CSS and flexbox',
+  //       link: '/workshops/responsive/index.html',
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: 'Accessibility in HTML and CSS',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/accessibility-prep.html',
+  //     },
+  //     {
+  //       text: 'Live coding',
+  //       link: '/lectures/accessibility-live-coding.html',
+  //     },
+  //     // {
+  //     //   text: 'Will it work? Using sandboxes to run code experiments',
+  //     //   link: '/lectures/will-it-work-using-sandboxes-to-test-code.html',
+  //     //   concepts: 'sandboxes'
+  //     // },
+  //     // {
+  //     //   text: 'Accessibility',
+  //     //   link: '/lectures/accessibility.html',
+  //     //   concepts: 'digital accessibility, disability, mainstream and assistive technology, standards',
+  //     //   code: ':focus outline header nav main section footer aria-label aria-labelledby'
+  //     // },
+  //     // {
+  //     //   text: 'Forms',
+  //     //   link: '/lectures/html-forms.html',
+  //     //   concepts: 'HTTP request methods, front end and back end',
+  //     //   code: 'form method action type label for input textarea name button'
+  //     // },
+  //     {
+  //       text: 'Workshop: Accessiblity',
+  //       link: '/workshops/accessibility/index.html',
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: 'Grids and other CSS concepts',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   //   // concepts: 'grids, positioning, z-index, navigation',
+  //   //   // code: 'grids positioning navigation'
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/grids-other-css-concepts.html',
+  //     },
+  //     {
+  //       text: 'Workshop: Grids and other CSS concepts',
+  //       link: '/workshops/grid/index.html',
 
-      }
-    ]
-  },
-  {
-    text: 'Advanced work with images',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    //   // concepts: 'grids, positioning, z-index, navigation',
-    //   // code: 'grids positioning navigation'
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/advanced-images.html',
-      },
-      {
-        text: 'Workshop: Advanced work with images',
-        link: '/workshops/advanced-images/index.html',
-      },
-      //    {
-      //      text: 'Workshop: Grids and other CSS concepts',
-      //      link: '/workshops/grid/index.html',
-      //    }
-    ]
-  },
-  {
-    text: 'Organizing your CSS',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/organizing-css-prep.html',
-      },
-      {
-        text: 'Live coding',
-        link: '/lectures/organizing-css-live-coding.html',
-      },
-      {
-        text: 'Workshop: Organizing your CSS',
-        link: '/workshops/organizing-css/index.html',
-      }
-    ]
-  },
-  {
-    text: 'JavaScript Values and Variables',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/javascript-values-variables.html',
-      },
-      {
-        text: 'Live Coding',
-        link: '/lectures/values-variables-live-coding.html',
-      },
-      {
-        text: 'Workshop: JavaScript Values and Variables',
-        link: '/workshops/values-variables/index.html',
-      },
-    ]
-  },
-  {
-    text: 'Conditions and functions',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/conditions-and-functions.html',
-      },
-      {
-        text: 'Conditionals',
-        link: '/lectures/conditionals.html',
-      },
-      {
-        text: 'Functions',
-        link: '/lectures/functions.html',
-      },
-      {
-      text: 'Workshop: Conditions and Functions',
-      link: '/workshops/conditions-functions/index.html',
-      },
-    ]
-  },
-  {
-    text: 'Objects and loops',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/objects-loops.html',
-      },
-      {
-        text: 'Live coding',
-        link: '/lectures/objects-loops-live-coding.html',
-      },
-      {
-        text: 'Workshop',
-        link: '/workshops/objects/index.html',
-      },
-    ]
-  },
-  {
-    text: 'The Document Object Model',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/dom.html',
-      },
-      {
-        text: 'Live coding',
-        link: '/lectures/dom-live-coding.html',
-      },
-      {
-        text: 'Workshop',
-        link: '/workshops/dom/index.html',
-      },
-    ]
-  },
-  {
-    text: 'Interaction and events',
-    collapsed: true,
-    lecturer: 'Joseph Muller',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/events.html',
-      },
-      {
-        text: 'Live coding',
-        link: '/lectures/events-live-coding.html',
-      },
-      {
-        text: 'Workshop',
-        link: '/workshops/events/index.html',
-      },
-    ]
-  },
-  {
-    text: 'Visualising Data',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/visualising-data.html',
-      },
-      {
-        text: 'Visualising Data',
-        link: '/lectures/visualising-data-js-1.html',
-      },
-      {
-        text: 'APIs and Fetching Data',
-        link: '/lectures/visualising-data-js-2.html',
-      },
-      {
-        text: 'Drawing shapes and text',
-        link: '/lectures/visualising-data-js-3.html',
-      },
-      {
-        text: 'Workshop',
-        link: '/workshops/data-vis/index.html',
-      },
-  ]
-  },
-  {
-    text: 'Animating Data',
-    collapsed: true,
-    lecturer: 'Helena Wee',
-    items: [
-      {
-        text: 'Prep',
-        link: '/lectures/animating-data.html',
-      },
-      {
-        text: 'Animating Data',
-        link: '/lectures/animating-data-1.html',
-      },
-      {
-        text: 'Workshop',
-        link: '/workshops/animating-data/index.html',
-      },
-  ]
-  },
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: 'Advanced work with images',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   //   // concepts: 'grids, positioning, z-index, navigation',
+  //   //   // code: 'grids positioning navigation'
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/advanced-images.html',
+  //     },
+  //     {
+  //       text: 'Workshop: Advanced work with images',
+  //       link: '/workshops/advanced-images/index.html',
+  //     },
+  //     //    {
+  //     //      text: 'Workshop: Grids and other CSS concepts',
+  //     //      link: '/workshops/grid/index.html',
+  //     //    }
+  //   ]
+  // },
+  // {
+  //   text: 'Organizing your CSS',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/organizing-css-prep.html',
+  //     },
+  //     {
+  //       text: 'Live coding',
+  //       link: '/lectures/organizing-css-live-coding.html',
+  //     },
+  //     {
+  //       text: 'Workshop: Organizing your CSS',
+  //       link: '/workshops/organizing-css/index.html',
+  //     }
+  //   ]
+  // },
+  // {
+  //   text: 'JavaScript Values and Variables',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/javascript-values-variables.html',
+  //     },
+  //     {
+  //       text: 'Live Coding',
+  //       link: '/lectures/values-variables-live-coding.html',
+  //     },
+  //     {
+  //       text: 'Workshop: JavaScript Values and Variables',
+  //       link: '/workshops/values-variables/index.html',
+  //     },
+  //   ]
+  // },
+  // {
+  //   text: 'Conditions and functions',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/conditions-and-functions.html',
+  //     },
+  //     {
+  //       text: 'Conditionals',
+  //       link: '/lectures/conditionals.html',
+  //     },
+  //     {
+  //       text: 'Functions',
+  //       link: '/lectures/functions.html',
+  //     },
+  //     {
+  //     text: 'Workshop: Conditions and Functions',
+  //     link: '/workshops/conditions-functions/index.html',
+  //     },
+  //   ]
+  // },
+  // {
+  //   text: 'Objects and loops',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/objects-loops.html',
+  //     },
+  //     {
+  //       text: 'Live coding',
+  //       link: '/lectures/objects-loops-live-coding.html',
+  //     },
+  //     {
+  //       text: 'Workshop',
+  //       link: '/workshops/objects/index.html',
+  //     },
+  //   ]
+  // },
+  // {
+  //   text: 'The Document Object Model',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/dom.html',
+  //     },
+  //     {
+  //       text: 'Live coding',
+  //       link: '/lectures/dom-live-coding.html',
+  //     },
+  //     {
+  //       text: 'Workshop',
+  //       link: '/workshops/dom/index.html',
+  //     },
+  //   ]
+  // },
+  // {
+  //   text: 'Interaction and events',
+  //   collapsed: true,
+  //   lecturer: 'Joseph Muller',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/events.html',
+  //     },
+  //     {
+  //       text: 'Live coding',
+  //       link: '/lectures/events-live-coding.html',
+  //     },
+  //     {
+  //       text: 'Workshop',
+  //       link: '/workshops/events/index.html',
+  //     },
+  //   ]
+  // },
+  // {
+  //   text: 'Visualising Data',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/visualising-data.html',
+  //     },
+  //     {
+  //       text: 'Visualising Data',
+  //       link: '/lectures/visualising-data-js-1.html',
+  //     },
+  //     {
+  //       text: 'APIs and Fetching Data',
+  //       link: '/lectures/visualising-data-js-2.html',
+  //     },
+  //     {
+  //       text: 'Drawing shapes and text',
+  //       link: '/lectures/visualising-data-js-3.html',
+  //     },
+  //     {
+  //       text: 'Workshop',
+  //       link: '/workshops/data-vis/index.html',
+  //     },
+  // ]
+  // },
+  // {
+  //   text: 'Animating Data',
+  //   collapsed: true,
+  //   lecturer: 'Helena Wee',
+  //   items: [
+  //     {
+  //       text: 'Prep',
+  //       link: '/lectures/animating-data.html',
+  //     },
+  //     {
+  //       text: 'Animating Data',
+  //       link: '/lectures/animating-data-1.html',
+  //     },
+  //     {
+  //       text: 'Workshop',
+  //       link: '/workshops/animating-data/index.html',
+  //     },
+  // ]
+  // },
   // {
   //   text: 'Timeouts, Debugging and Packages',
   //   lecturer: 'Helena Wee and Joseph Muller',
