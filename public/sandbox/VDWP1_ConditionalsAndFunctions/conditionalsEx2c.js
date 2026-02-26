d08a36d254400caac100cfe6e5f4669abb41ac5e
Helena Wee <h.wee@bbk.ac.uk>
2026-02-26 23:36:00 +0000

    added code to check if prompt input is a number

--- public/sandbox/VDWP1_ConditionalsAndFunctions/conditionalsEx2c.js
+++ public/sandbox/VDWP1_ConditionalsAndFunctions/conditionalsEx2c.js
@@ -1,10 +1,14 @@
 // this example runs in browser console
 let num = Number(prompt("Pick a number"));
 
-if (num < 10) {
-  console.log("Small");
-} else if (num < 100) {
-  console.log("Medium");
+if(!isNaN(num)){
+  if (num < 10) {
+    console.log("Small");
+  } else if (num < 100) {
+    console.log("Medium");
+  } else {
+    console.log("Large");
+  }
 } else {
-  console.log("Large");
+  console.log("You did not type in a number")
 }
