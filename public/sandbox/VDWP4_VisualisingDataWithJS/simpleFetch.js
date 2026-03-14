f95be4efe2796aad0e66260ee3c34b2ccc1f5c4d
Helena Wee <h.wee@bbk.ac.uk>
2026-03-14 00:01:39 +0000

    changed simpleFetch.js

--- public/sandbox/VDWP4_VisualisingDataWithJS/simpleFetch.js
+++ public/sandbox/VDWP4_VisualisingDataWithJS/simpleFetch.js
@@ -1,15 +1,9 @@
-//setTimeout(function() {
-    getText("https://temp.staticsave.com/67d53f98ba08b.txt");
-//}, 8000);
-/*
-setTimeout(getTxt(), 8000);
-
-function getTxt(){
-  getText("https://temp.staticsave.com/65d8d1d3d272f.txt");
-}
-*/
 async function getText(file) {
   let myObject = await fetch(file);     // myObject is the response object of the fetch()
   let myText = await myObject.text();     // text() method of response object returns a promise that resolves to a string
   document.getElementById("demo").innerHTML = myText;
 }
+
+//setTimeout(function() {
+    getText("https://temp.staticsave.com/69b491b0ec093.css");
+//}, 8000);
